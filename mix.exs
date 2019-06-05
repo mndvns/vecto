f10694ef8a160ea674be37666cf8b571c9846e22
Michael Vanasse <michaelvanasse@gmail.com>
2019-06-05 07:10:08 -0600

    release 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vecto.MixProject do
   def project do
     [
       app: :vecto,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       deps: deps(),
       start_permanent: Mix.env() == :prod,
